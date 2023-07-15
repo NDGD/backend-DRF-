a9ab7da771260a093b777be7692e027c97210dc0
KyungBin7 <jkb12377@naver.com>
2023-07-16 00:41:16 +0900

    if login issue check fix code

--- api/accounts/views.py
+++ api/accounts/views.py
@@ -8,8 +8,22 @@ class UserCreate(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
-# 로그인
-class UserLogin(generics.RetrieveAPIView):
-    queryset = User.objects.all()
+
+#만약 로그인기능 잘 안될시
+'''
+from django.contrib.auth import authenticate, login
+from django.http import JsonResponse
+
+class UserLogin(generics.GenericAPIView):
     serializer_class = UserSerializer
 
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return JsonResponse({'message': '로그인 성공'})
+        else:
+            return JsonResponse({'message': '로그인 실패'})'''
